b6091334daad0ebc97ecb6c71ac9f2d4408e3bd9
tyk2013 <ken.rad0202@gmail.com>
2016-07-07 13:23:46 +0900

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'okano' });
+  res.render('index', { title: 'kenzo okano' });
 });
 
 module.exports = router;
