78389ee153b656054f615ca8ffcfd5927e5d23d9
tyk2013 <ken.rad0202@gmail.com>
2016-07-07 13:25:10 +0900

    intial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'kenzo okano' });
+  res.render('index', { title: 'kenzo' });
 });
 
 module.exports = router;
