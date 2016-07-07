d168d2c97877ec1d88538c4deb4fe2bc8e0ea325
tyk2013 <ken.rad0202@gmail.com>
2016-07-07 13:45:09 +0900

    test3 commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'take' });
+  res.render('index', { title: 'taketaketake' });
 });
 
 module.exports = router;
