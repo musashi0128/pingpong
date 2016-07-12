fca35cf673f09e6d64ba89220fd862e528ec88e9
KazuhikoMiyamoto <k.miyamoto.0128@gmail.com>
2016-07-12 15:13:12 +0900

    commit root/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'taketaketake' });
+  res.render('index', { title: 'take' });
+});
+router.get('/room',function (req,res,next) {
+  res.render('room',{title:"部屋"});
+});
+
+router.get('/owner',function (req,res,next) {
+  res.render('room_owner',{title:"管理者"});
+});
+router.get('/vote_make',function (req,res,next) {
+  res.render('vote_make',{title:"アンケート作成"});
+});
+router.get('/member',function (req,res,next) {
+  res.render('room_member',{title:"参加者"});
+});
+router.get('/entry',function (req,res,next) {
+  res.render('room',{title:"投票"});
 });
 
 module.exports = router;
